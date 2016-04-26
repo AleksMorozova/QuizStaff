6eb71d062585bfb81984308b03dcf1705f95fd4e
AleksMorozova <aleks_morozova91@mail.ru>
2016-04-26 17:30:02 +0300

    First iteration for creation of main form

---
+++ QuizStaff/Client/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using DevExpress.XtraBars;
+
+namespace Client
+{
+    public partial class MainForm : DevExpress.XtraEditors.XtraForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void barButtonItem1_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Form f = new Form();
+            f.Text = "Child Form ";
+            f.MdiParent = this;
+            f.Show();
+        }
+    }
+}
