225c8f16f33cc673c72ffc08a1f64a5ee3cf34af
lobandml <loband_ml@ukr.net>
2016-04-26 18:26:14 +0300

    domain model

--- QuizStaff/DomainModel/Testee.cs
+++ QuizStaff/DomainModel/Testee.cs
@@ -8,5 +8,26 @@ namespace DomainModel
 {
     public class Testee
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private string email;
+        private string login;
+        private string password;
+
+        List<History> Histories { get; set; }
+        List<Training> Trainings { get; set; }
+
+        public Testee(string firstname, string lastname, string email)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.email = email;
+
+            login = string.Empty;
+            password = string.Empty;
+
+            Histories = new List<History>();
+            Trainings = new List<Training>();
+        }
     }
 }
