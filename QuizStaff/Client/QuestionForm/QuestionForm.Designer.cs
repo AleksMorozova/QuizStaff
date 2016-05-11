31fb4606081167f3a082f8a5033a772c83cbc6ae
nashilnik <bogdan.nashilnik@gmail.com>
2016-05-11 19:04:37 +0300

    Delete mock file. Bug fixes. Minor change in UI and logic.

--- QuizStaff/Client/QuestionForm/QuestionForm.Designer.cs
+++ QuizStaff/Client/QuestionForm/QuestionForm.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.flow = new System.Windows.Forms.FlowLayoutPanel();
-            this.labelQuestion = new Client.WrappingLabel();
             this.buttonSend = new DevExpress.XtraEditors.SimpleButton();
+            this.labelQuestion = new Client.WrappingLabel();
             this.flow.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -39,12 +39,23 @@
             this.flow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.flow.AutoScroll = true;
             this.flow.Controls.Add(this.labelQuestion);
             this.flow.Location = new System.Drawing.Point(0, 0);
             this.flow.Name = "flow";
             this.flow.Padding = new System.Windows.Forms.Padding(20);
-            this.flow.Size = new System.Drawing.Size(372, 410);
+            this.flow.Size = new System.Drawing.Size(383, 410);
             this.flow.TabIndex = 0;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSend.Location = new System.Drawing.Point(276, 416);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(96, 33);
+            this.buttonSend.TabIndex = 1;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
             // 
             // labelQuestion
             // 
@@ -57,16 +68,6 @@
             this.labelQuestion.TabIndex = 0;
             this.labelQuestion.Text = "Question";
             // 
-            // buttonSend
-            // 
-            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonSend.Location = new System.Drawing.Point(276, 416);
-            this.buttonSend.Name = "buttonSend";
-            this.buttonSend.Size = new System.Drawing.Size(96, 33);
-            this.buttonSend.TabIndex = 1;
-            this.buttonSend.Text = "Send";
-            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
-            // 
             // QuestionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -78,6 +79,7 @@
             this.Name = "QuestionForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Question";
+            this.Load += new System.EventHandler(this.QuestionForm_Load);
             this.flow.ResumeLayout(false);
             this.flow.PerformLayout();
             this.ResumeLayout(false);
