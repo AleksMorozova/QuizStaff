a845e416cef1747f6a3c87688dc90b603f7d67f1
AleksMorozova <aleks_morozova91@mail.ru>
2016-06-23 16:03:19 +0300

    add Settings form

---
+++ QuizStaff/TesteeApplication/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using TesteeApplication.TesteeSettings;
+using System.Configuration;
+
+namespace TesteeApplication
+{
+    public partial class MainForm : DevExpress.XtraEditors.XtraForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void settingsBarButtonItem_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            TesteeSettingsForm newMDIChild = new TesteeSettingsForm();
+            newMDIChild.Text = "Settings";
+            newMDIChild.WindowState = FormWindowState.Maximized;
+            newMDIChild.MdiParent = this;
+            newMDIChild.Show();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(System.Windows.Forms.Application.ExecutablePath);
+            config.AppSettings.Settings.Remove("Lang");
+            config.AppSettings.Settings.Add("Lang", Program.currentLang);
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+    }
+}
---
+++ QuizStaff/TesteeApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TesteeApplication
+{
+    static class Program
+    {
+        private static MainForm applicationMainForm;
+        public static string currentLang = "ru-RU";
+        public static MainForm ApplicationMainForm { get { return applicationMainForm; } }
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            applicationMainForm = new MainForm();
+            Application.Run(new MainForm());
+        }
+    }
+}
