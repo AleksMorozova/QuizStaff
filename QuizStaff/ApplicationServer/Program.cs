a0d82a7a33dacf018300a5abdd60d0d375bb6748
AleksMorozova <aleks_morozova91@mail.ru>
2016-09-14 15:16:07 +0300

    Remove comments

--- QuizStaff/ApplicationServer/Program.cs
+++ QuizStaff/ApplicationServer/Program.cs
@@ -13,25 +13,20 @@ namespace ApplicationServer
         static void Main(string[] args)
         {
             Uri baseAddress = new Uri("http://dws01:8733/ApplicationServer");
-            // Create the ServiceHost.
+
             using (ServiceHost host = new ServiceHost(typeof(ApplicationServer), baseAddress))
             {
-                // Enable metadata publishing.
                 ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
                 smb.HttpGetEnabled = true;
                 smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
                 host.Description.Behaviors.Add(smb);
 
-                // Open the ServiceHost to start listening for messages. Since
-                // no endpoints are explicitly configured, the runtime will create
-                // one endpoint per base address for each service contract implemented
-                // by the service.
                 host.Open();
                 
                 Console.WriteLine("The service is ready at {0}", baseAddress);
                 Console.WriteLine("Press <Enter> to stop the service.");
                 Console.ReadLine();
-                // Close the ServiceHost.
+
                 host.Close();
             }
         }
