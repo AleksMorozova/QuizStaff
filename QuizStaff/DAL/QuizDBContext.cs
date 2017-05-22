20718649fbd4606b882ebd8cde6790c7dc7d3436
AleksMorozova <aleks_morozova91@mail.ru>
2017-05-22 17:21:45 +0300

    Add migrations

--- QuizStaff/DAL/QuizDBContext.cs
+++ QuizStaff/DAL/QuizDBContext.cs
@@ -13,7 +13,7 @@ namespace DAL
         public QuizDBContext()
             : base()
         {
-            Database.CreateIfNotExists();
+            Database.SetInitializer<QuizDBContext>(new QuizDBInitializer());
         }
 
         public DbSet<Testee> Testees { get; set; }
---
+++ QuizStaff/DAL/QuizDBInitializer.cs
@@ -0,0 +1,62 @@
+﻿using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class QuizDBInitializer : CreateDatabaseIfNotExists<QuizDBContext>
+    {
+        protected override void Seed(QuizDBContext context)
+        {
+            #region add permissions
+            Permission createAdministrator = new Permission() { Title = "Create administrator", Type = PermissionType.CreateAdministrator };
+            context.Permissions.Add(createAdministrator);
+
+            Permission editSetting = new Permission() { Title = "Edit setting", Type = PermissionType.EditSetUp };
+            context.Permissions.Add(editSetting);
+
+            Permission editTestee = new Permission() { Title = "Edit testee", Type = PermissionType.EditTestee };
+            context.Permissions.Add(editTestee);
+
+            Permission editTraining = new Permission() { Title = "Edit training", Type = PermissionType.EditTraining };
+            context.Permissions.Add(editTraining);
+
+            Permission getQuestion = new Permission() { Title = "Get question", Type = PermissionType.GetQuestion };
+            context.Permissions.Add(getQuestion);
+
+            context.SaveChanges();
+            #endregion
+
+            #region add roles
+            Role mainAdministratorRole = new Role() { Name = "Main Admin", Description = "" };
+            context.Roles.Add(mainAdministratorRole);
+
+            Role administratorRole = new Role() { Name = "Admin", Description =""};
+            context.Roles.Add(administratorRole);
+
+            Role testeeRole = new Role() { Name = "Testee", Description = "" };
+            context.Roles.Add(testeeRole);
+
+            context.SaveChanges();
+            #endregion
+
+            RolePermission p1 = new RolePermission() { Role = administratorRole, Permission = editSetting };
+            context.RolePermissions.Add(p1);
+            RolePermission p2 = new RolePermission() { Role = administratorRole, Permission = editTestee };
+            context.RolePermissions.Add(p2);
+            RolePermission p3 = new RolePermission() { Role = administratorRole, Permission = editTraining };
+            context.RolePermissions.Add(p3);
+            RolePermission p4 = new RolePermission() { Role = testeeRole, Permission = getQuestion };
+            context.RolePermissions.Add(p4);
+            RolePermission p5 = new RolePermission() { Role = mainAdministratorRole, Permission = createAdministrator };
+            context.RolePermissions.Add(p5);
+
+            context.SaveChanges();
+            base.Seed(context);
+        }
+    }
+}
