c61e0cfda8280bfd6c778c455f30db027f0b3e4d
AleksMorozova <aleks_morozova91@mail.ru>
2017-05-24 12:57:14 +0300

    add service reference + update migration

--- QuizStaff/LoadDataFromEPE/EPELoader.cs
+++ QuizStaff/LoadDataFromEPE/EPELoader.cs
@@ -11,34 +11,34 @@ namespace LoadDataFromEPE
     {
         public static List<Testee> LoadTesteeFromEPE()
         {
-            List<Testee> loadedTesteess = new List<Testee>();
+            //List<Testee> loadedTesteess = new List<Testee>();
             //TODO: Add epe service reference 
-            //var service = new EPEServiceReference.IsdDataWebServiceSoapClient();
-            //var testees = service.GetIsdEmployeeOfficeData();
-            //
+            var service = new EPEServiceReference.IsdDataWebServiceSoapClient();
+            var testees = service.GetIsdEmployeeOfficeData();
 
-            //for (int i = 0; i < testees.Tables[0].Rows.Count; i++)
-            //{
-            //    Testee newTestee = new Testee();
-            //    newTestee.Attribute1 = testees.Tables[0].Rows[i]["Company"].ToString();
-            //    newTestee.Attribute2 = testees.Tables[0].Rows[i]["OfficeLoc"].ToString();
-            //    newTestee.Login = testees.Tables[0].Rows[i]["Login"].ToString();
-            //    newTestee.LastName = testees.Tables[0].Rows[i]["LastName"].ToString();
-            //    newTestee.FirstName = testees.Tables[0].Rows[i]["FirstName"].ToString();
-            //    newTestee.Attribute3 = testees.Tables[0].Rows[i]["Sector"].ToString();
-            //    newTestee.Attribute4 = testees.Tables[0].Rows[i]["Division"].ToString();
-            //    newTestee.Attribute5 = testees.Tables[0].Rows[i]["Department"].ToString();
-            //    newTestee.Attribute6 = testees.Tables[0].Rows[i]["Profession"].ToString();
-            //    newTestee.Attribute7 = testees.Tables[0].Rows[i]["Title"].ToString();
-            //    newTestee.Attribute8 = testees.Tables[0].Rows[i]["Position"].ToString();
-            //    newTestee.Attribute9 = testees.Tables[0].Rows[i]["Role"].ToString();
-            //    newTestee.Attribute10 = testees.Tables[0].Rows[i]["Manager"].ToString();
-            //    newTestee.Attribute11 = testees.Tables[0].Rows[i]["HireDate"].ToString();
-            //    newTestee.Attribute12 = testees.Tables[0].Rows[i]["Com_Exp"].ToString();
-            //    newTestee.Attribute13 = testees.Tables[0].Rows[i]["ISD_Exp"].ToString();
 
-            //    loadedTesteess.Add(newTestee);
-            //}
+            for (int i = 0; i < testees.Tables[0].Rows.Count; i++)
+            {
+                Testee newTestee = new Testee();
+                newTestee.Attribute1 = testees.Tables[0].Rows[i]["Company"].ToString();
+                newTestee.Attribute2 = testees.Tables[0].Rows[i]["OfficeLoc"].ToString();
+                newTestee.Login = testees.Tables[0].Rows[i]["Login"].ToString();
+                newTestee.LastName = testees.Tables[0].Rows[i]["LastName"].ToString();
+                newTestee.FirstName = testees.Tables[0].Rows[i]["FirstName"].ToString();
+                newTestee.Attribute3 = testees.Tables[0].Rows[i]["Sector"].ToString();
+                newTestee.Attribute4 = testees.Tables[0].Rows[i]["Division"].ToString();
+                newTestee.Attribute5 = testees.Tables[0].Rows[i]["Department"].ToString();
+                newTestee.Attribute6 = testees.Tables[0].Rows[i]["Profession"].ToString();
+                newTestee.Attribute7 = testees.Tables[0].Rows[i]["Title"].ToString();
+                newTestee.Attribute8 = testees.Tables[0].Rows[i]["Position"].ToString();
+                newTestee.Attribute9 = testees.Tables[0].Rows[i]["Role"].ToString();
+                newTestee.Attribute10 = testees.Tables[0].Rows[i]["Manager"].ToString();
+                newTestee.Attribute11 = testees.Tables[0].Rows[i]["HireDate"].ToString();
+                newTestee.Attribute12 = testees.Tables[0].Rows[i]["Com_Exp"].ToString();
+                newTestee.Attribute13 = testees.Tables[0].Rows[i]["ISD_Exp"].ToString();
+
+                loadedTesteess.Add(newTestee);
+            }
 
             return loadedTesteess;
         }
